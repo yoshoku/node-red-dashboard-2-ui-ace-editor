4ef16395b55aa101d00d1e331d7ec7b3be1dc4f5
yoshoku <yoshoku@outlook.com>
2025-08-23 13:39:19 +0900

    feat: add v-ace-editor

--- ui/components/UIAceEditor.vue
+++ ui/components/UIAceEditor.vue
@@ -1,103 +1,31 @@
 <template>
     <!-- Component must be wrapped in a block so props such as className and style can be passed in from parent -->
     <div className="ui-ace-editor-wrapper">
-        <h1>Example Integrated Widget: {{ id }}</h1>
-
-        <!-- Note: We can use any Vuetify Components by default -->
-        <h2>Using Vuetify Components</h2>
-        <p>
-            <a href="https://vuetifyjs.com/en/components/all/#containment" target="_blank">Vuetify</a> is included by default, so we can include any of their components.
-            Here we have a button that will open a "Hello World" alert:
-        </p>
-        <pre>&lt;v-btn @click="alert('Hello World')"&gt;Alert "Hello World"&lt;/v-btn&gt;</pre>
-        <v-btn @click="alert('Hello World')">Alert "Hello World"</v-btn>
-
-        <h2>Accessing Properties</h2>
-        <p>
-            Your Vue component will be rendered and passed a few built-in properties.
-        </p>
-
-        <h3><code>this.id</code></h3>
-        <p>Each widget has a unique ID, which can be used when sending messages to Node-RED via <code>this.$socket.emit()</code>.</p>
-        <pre>{{ id }}</pre>
-
-        <h3><code>this.props</code></h3>
-        <p>
-            The <code>props</code> object contains the properties defined in the widget's configuration in Node-RED.
-            It is a snapshot of the properties at the time the widget was loaded
-        </p>
-        <pre>{{ props }}</pre>
-
-        <h3><code>this.state</code></h3>
-        <p>
-            The <code>state</code> object contains any properties that have been overridden at runtime (after the node is deployed).
-            Your node should support override of configuration options via <code>msg.ui_update.&lt;property&gt;</code>
-        </p>
-        <pre>{{ state }}</pre>
-
-        <h2>Dynamic Properties</h2>
-        <p>We have setup a dynamic property <code>example</code> which can be updated from Node-RED.</p>
-        <p>
-            We've wrapped this into a computed variable and used the built-in <code>getProperty()</code>
-            function which automatically checks both <code>props</code> and <code>state</code>.
-        </p>
-        <pre>{{ example }}</pre>
-
-        <h2>Communications with Node-RED</h2>
-        <p>Events are sent back and forth between Node-RED and Dashboard 2.0 with SocketIO</p>
-        <h3>Sending Node-RED Messages</h3>
-        <p>You can send and receive the built-in events, as well as defining your own:</p>
-        <v-btn @click="send({payload: 'Hello World'})">Send msg</v-btn>
-        <h3>Sending Custom SocketIO Events</h3>
-        <p>
-            You can also send custom events, which can be handled by a custom event handler in Node-RED.
-            The Node-RED-side's handler should have <code>(conn, id, msg)</code> as the input variables.
-        </p>
-        <v-btn @click="test()">Send 'my-custom-event'</v-btn>
-
-        <h2>Data Retention &amp; VueX Stores</h2>
-        <p>Dashboard 2.0 has a built-in VueX datastore. This can be used to store (and retrieve) the latest received messages.</p>
-        <p>Note: the vuex store is cleared on refresh of a screen, at which point, data will be loaded from the Node-RED datastore, should it be present.</p>
-        <p>Send a message to this node in order to see the value here:</p>
-        <pre>{{ messages && messages[id] ? messages[id] : 'No Data' }}</pre>
-        <p>Note that it persists, even after refresh. This is because, in our <code>onInput</code> event handler in our <code>ui-example.js</code> file, we store the message in the Node-RED datastore.</p>
-
-        <!-- Note: We can use any Vuetify Components by default -->
-        <h2>Styling with Vuetify &amp; CSS</h2>
-        <p>
-            We can define our own CSS within the widget's repository,
-            and expose classes like <code class="ui-ace-editor-class">ui-ace-editor-class</code>
-        </p>
-        <p>
-            Vuetify also comes with a handful of utility classes to assist with styling:
-            <ul>
-                <li v-for="guide in vuetifyStyles" :key="guide.label"><a :href="guide.url" target="_blank">{{ guide.label }}</a></li>
-            </ul>
-        </p>
-
-        <h2>External Dependencies</h2>
-        <p>
-            Any additional libraries you require can be installed as in any other package.
-            When the <code>npm run build</code> command is run, Vite will package everything
-            up into the single <code>.umd.js</code> file.
-        </p>
-        <p>
-            Here, we include the NPM package <code>to-title-case</code>, and bind it's
-            functionality to a VueJS Computed Variable, which automatically re-calculates.
-        </p>
-        <p>
-            <v-text-field v-model="input.title" type="text" />
-            VueJS Computed Variable Output: <i>{{ titleCase }}</i>
-        </p>
+        <v-ace-editor
+            v-model:value="content"
+            lang="javascript"
+            theme="github"
+            style="height: 100%;"
+            @init="editorInit"
+        />
     </div>
 </template>
 
 <script>
-import toTitleCase from 'to-title-case'
+import ace from 'ace-builds'
+import modeJavascriptUrl from 'ace-builds/src-noconflict/mode-javascript'
+import themeGitHubUrl from 'ace-builds/src-noconflict/theme-github'
+import { VAceEditor } from 'vue3-ace-editor'
 import { mapState } from 'vuex'
+
+ace.config.setModuleUrl('ace/mode/javascript', modeJavascriptUrl)
+ace.config.setModuleUrl('ace/theme/github', themeGitHubUrl)
 
 export default {
     name: 'UIAceEditor',
+    components: {
+        VAceEditor
+    },
     inject: ['$socket', '$dataTracker'],
     props: {
         /* do not remove entries from this - Dashboard's Layout Manager's will pass this data to your component */
@@ -122,9 +50,6 @@ export default {
         }
     },
     computed: {
-        titleCase () {
-            return toTitleCase(this.input.title)
-        },
         ...mapState('data', ['messages']),
         // wrap our "example" property in a computed property to ensure it re-calculates when the property changes
         example () {
@@ -197,4 +122,10 @@ export default {
 </script>
 
 <style scoped>
+.ui-ace-editor-wrapper {
+    padding: 0;
+    margin: 0;
+    border: 1px solid #d1d1d1;
+    border-radius: 4px;
+}
 </style>
